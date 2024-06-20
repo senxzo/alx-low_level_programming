98036a988a3d0c45d60071ea31891dd88d1d9006
Innocent Audu <innocentaudu2821@gmail.com>
2024-06-20 15:56:44 -0700

    c basics

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It prints a message to the console.
+ *
+ * Return: Always returns 0 (success).
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
