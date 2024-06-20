872db0fecb9d452620fff0c98963161635884e14
Innocent Audu <innocentaudu2821@gmail.com>
2024-06-20 16:23:03 -0700

    c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,6 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, message, 59);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
