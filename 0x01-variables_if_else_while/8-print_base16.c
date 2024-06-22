d9596530a6d7ddcaa123862a813da079e8295aa2
senxzo <114511444+senxzo@users.noreply.github.com>
2024-06-22 12:58:07 -0500

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,22 +10,22 @@
  */
 int main(void)
 {
-    char digit;
+char digit;
 
-    // Print digits '0' to '9'
-    for (digit = '0'; digit <= '9'; digit++)
-    {
-        putchar(digit);
-    }
+/* Print digits '0' to '9' */
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
 
-    // Print lowercase letters 'a' to 'f'
-    for (digit = 'a'; digit <= 'f'; digit++)
-    {
-        putchar(digit);
-    }
+/* Print lowercase letters 'a' to 'f' */
+for (digit = 'a'; digit <= 'f'; digit++)
+{
+putchar(digit);
+}
 
-    // Print a newline character
-    putchar('\n');
+/* Print a newline character */
+putchar('\n');
 
-    return (0);
+return (0);
 }
