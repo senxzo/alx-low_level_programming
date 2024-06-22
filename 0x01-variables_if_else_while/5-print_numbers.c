f3f9f318a0d1a0ee92e6b82f0cfac6770fa9da36
senxzo <114511444+senxzo@users.noreply.github.com>
2024-06-22 12:51:48 -0500

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,14 +10,14 @@
  */
 int main(void)
 {
-    char digit;
+char digit;
 
-    for (digit = '0'; digit <= '9'; digit++)
-    {
-        putchar(digit);
-    }
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
