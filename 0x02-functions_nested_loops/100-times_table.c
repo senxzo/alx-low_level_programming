063b4077775fecdcd798c59e6e50d335637ede0a
senxzo <innocentaudu2821@gmail.com>
2024-06-26 18:45:49 -0500

    C-functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,28 +6,28 @@
  */
 void print_times_table(int n)
 {
-    int i, j, product;
+	int i, j, product;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			product = i * j;
 
-            if (j != 0)
-            {
-                printf(", ");
-                if (product < 10)
-                    printf("  ");
-                else if (product < 100)
-                    printf(" ");
-            }
+			if (j != 0)
+			{
+				printf(", ");
+				if (product < 10)
+					printf("  ");
+				else if (product < 100)
+					printf(" ");
+			}
 
-            printf("%d", product);
-        }
-        printf("\n");
-    }
+			printf("%d", product);
+		}
+		printf("\n");
+	}
 }
