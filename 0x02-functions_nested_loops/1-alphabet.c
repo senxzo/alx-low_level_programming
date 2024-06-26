3f45605bae38576fc1cf335e6824c2f5f237d73e
senxzo <innocentaudu2821@gmail.com>
2024-06-25 22:50:19 -0500

    C-functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,17 @@
  * main - Entry point
  *
  * Description: Prints alphabets followed by a new line.
+ * print_alphabet - Prints the alphabet in lowercase,
  *
  * Return: Always 0 (Success)
  */
  void print_alphabet(void)
- {
- char letter;
+{
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
-    _putchar('\n');
- }
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
