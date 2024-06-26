0218e66e00a055b0548435ebb153d19e77687617
senxzo <innocentaudu2821@gmail.com>
2024-06-26 05:01:01 -0500

    C-functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,22 @@ int print_last_digit(int n)
 {
     int last_digit;
 
-    if (n < 0)
-        last_digit = -n % 10; 
+    if (n == INT_MIN)
+    {
+        last_digit = 8; 
+        _putchar('0' + last_digit);
+        return last_digit;
+    }
+    else if (n < 0)
+    {
+        last_digit = -n % 10;  
+    }
     else
+    {
         last_digit = n % 10;  
+    }
 
-    _putchar('0' + last_digit);
+    _putchar('0' + last_digit); 
 
-    return last_digit;
+    return last_digit; 
 }
