6c90beb8bb4eebb18731ed6804adacc30bc44d26
senxzo <innocentaudu2821@gmail.com>
2024-06-26 05:46:17 -0500

    C-functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,18 @@
  */
 void jack_bauer(void)
 {
-    int hours, minutes;
+	int hours, minutes;
 
-    for (hours = 0; hours < 24; hours++)
-    {
-        for (minutes = 0; minutes < 60; minutes++)
-        {
-            _putchar((hours / 10) + '0');
-            _putchar((hours % 10) + '0');
-            _putchar(':');
-            _putchar((minutes / 10) + '0');
-            _putchar((minutes % 10) + '0');
-            _putchar('\n');
-        }
-    }
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
