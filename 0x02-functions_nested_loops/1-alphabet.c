7810e4ca755a1723fb87fde51f495acb6c998297
senxzo <innocentaudu2821@gmail.com>
2024-06-25 22:55:36 -0500

    C-functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints alphabets followed by a new line.
+ * Calls the function to print the alphabet.
  *
- * print_alphabet - Prints the alphabet in lowercase,
- * followed by a new line.
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
