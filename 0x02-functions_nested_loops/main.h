0854e07e35bc6bad26b8d7357f7892328599a76b
senxzo <innocentaudu2821@gmail.com>
2024-06-26 04:12:57 -0500

    C-functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c);
 void print_alphabet(void); /* Function prototype for print_alphabet */
 void print_alphabet_x10(void); /* prints 10 times the alphabet */
+int _islower(int c);
 
 #endif /* MAIN_H */
