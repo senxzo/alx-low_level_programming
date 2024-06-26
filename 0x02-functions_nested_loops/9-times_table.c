50ee32ec7cb01e8485dfecc3263bbbbf6887b7e1
senxzo <innocentaudu2821@gmail.com>
2024-06-26 05:49:52 -0500

    C-functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,30 +5,30 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
-            if (product < 10)
-            {
-                if (column > 0)
-                    _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-            if (column < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			if (product < 10)
+			{
+				if (column > 0)
+					_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			if (column < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
