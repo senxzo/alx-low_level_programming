085812762a31d04b4f9287d039f9eabf30ae4022
senxzo <innocentaudu2821@gmail.com>
2024-06-26 04:22:35 -0500

    C-functions

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_isalpha: check if character is alpha
+*@c: character to be checked
+*
+* Return: 1 for alpha and 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z')
+    Return(1);
+else
+    Return(0);
+}
