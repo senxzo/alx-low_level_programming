88d7cc299ec0815ff7f9f6877b99c510dd35129e
senxzo <innocentaudu2821@gmail.com>
2024-07-02 04:21:45 -0500

    c functions and pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates the value of the integer it points to to 98.
+ * @n: Pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
+
+int main(void)
+{
+    int value = 42;
+    printf("Before: %d\n", value);
+    reset_to_98(&value);
+    printf("After: %d\n", value);
+    return 0;
+}
