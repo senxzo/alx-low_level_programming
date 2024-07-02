450e15b9d7e3edeafdf49e24d93da1c2ad153355
senxzo <innocentaudu2821@gmail.com>
2024-07-02 13:07:57 -0500

    c functions and pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,11 @@
 #include "main.h"
-
 /**
- * reset_to_98 - Updates the value of the integer it points to to 98.
- * @n: Pointer to an int.
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Return: 0 is success
+*/
+
 void reset_to_98(int *n)
 {
-    *n = 98;
-}
-
-int main(void)
-{
-    int value = 42;
-    printf("Before: %d\n", value);
-    reset_to_98(&value);
-    printf("After: %d\n", value);
-    return 0;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,27 +1,15 @@
 #include "main.h"
-
 /**
- * swap_int - Swaps the values of two integers.
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
- */
+* swap_int - swaps the value of int a and int b
+* @a: first int
+* @b: second int
+* Return 0
+*/
 void swap_int(int *a, int *b)
 {
-    int temp;
+		int s;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int main(void)
-{
-    int x = 42;
-    int y = 98;
-
-    printf("Before swap: x = %d, y = %d\n", x, y);
-    swap_int(&x, &y);
-    printf("After swap: x = %d, y = %d\n", x, y);
-
-    return 0;
+		s = *a;
+		*a = *b;
+		*b = s;
 }
