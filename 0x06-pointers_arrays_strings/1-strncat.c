41f7365b27b1f9463465d135b6c28c2f20b4464e
senxzo <innocentaudu2821@gmail.com>
2024-07-04 15:51:09 -0500

    more c functions and pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strncat - function
+* @dest: param 1
+* @src: param 2
+* @n: param 3
+* return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+    int i;
+    int destcount =0;
+    for (i = 0; dest[i] != '\0'; i++)
+    {
+    destcount++;
+    }
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[destcount] = src[i];
+        destcount++;
+    }
+     dest[destcount] = '\0';
+
+    return dest;
+    
+}
