4b46c3fafb3473b481f0f170f10816c630231fb8
senxzo <innocentaudu2821@gmail.com>
2024-07-04 13:08:30 -0500

    more c functions and pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *s = dest;
+	char *deststr = dest;
 
 	while (*dest != '\0')
 	{
@@ -22,5 +22,5 @@ char *_strcat(char *dest, char *src)
 		src++;
 	}
 	*dest = '\0';
-	return (s);
+	return (deststr);
 }
