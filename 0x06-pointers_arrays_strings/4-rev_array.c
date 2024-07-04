6c5fd55e3612e6e690f6a3219509162951b51b46
senxzo <innocentaudu2821@gmail.com>
2024-07-04 18:00:51 -0500

    more c functions and pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,8 @@ void reverse_array(int *a, int n)
 {
     int i, temp;
 
-    // Loop through the array from the end to the beginning
     for (i = n - 1; i >= 0; i--)
     {
-        // Swap elements between start and end
         temp = a[i];
         a[i] = a[n - 1 - i];
         a[n - 1 - i] = temp;
