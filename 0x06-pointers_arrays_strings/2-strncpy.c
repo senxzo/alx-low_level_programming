7723e97168b8beccaa0a1137317c40da56ace0f3
senxzo <innocentaudu2821@gmail.com>
2024-07-04 16:58:09 -0500

    more c functions and pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * _strncat - Copies up to n characters from the src string to the dest string
+ * _strncpy - Copies up to n characters from the src string to the dest string
  *
- * @src: The source of strings
  * @dest: The destination of the string
- * @n: The length of int
+ * @src: The source of strings
+ * @n: The maximum number of characters to copy
  *
  * Return: pointer to the resulting string dest
  */
- char *_strncpy(char *dest, char *src, int n)
- {
-     int i;
-     for (i = 0; i < n && src[i] != '\0'; i++)
+char *_strncpy(char *dest, const char *src, int n)
+{
+    int i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
     {
         dest[i] = src[i];
     }
+
     for (; i < n; i++)
     {
         dest[i] = '\0';
     }
-    return (dest);
- }
- 
+
+    return dest;
+}
