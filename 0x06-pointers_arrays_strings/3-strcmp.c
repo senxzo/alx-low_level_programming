a436ab7d8e1a3125ce06c8e286edfcee65cc9e3f
senxzo <innocentaudu2821@gmail.com>
2024-07-04 17:53:41 -0500

    more c functions and pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-    while (s1[i] == s2[i])
-    {
-        if (s1[i] == '\0')
-            return 0; 
-        i++;
-    }
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
 
-    return s1[i] - s2[i];
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
