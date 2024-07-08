f26abd38aade664e3870855d65cf1cdafe76c359
senxzo <innocentaudu2821@gmail.com>
2024-07-08 17:59:38 -0500

    more c functions and pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
-* _memcpy - function name
-* @dest: param 1
-* @src: param 2
-* @n: param 3
-* return: dest
-*/
+ * _memcpy - function copies @n bytes from memory area @src
+ * to memory area @dest
+ *
+ * @n: function copies
+ *
+ * @src: bytes from memory area
+ *
+ * @dest: to memory area
+ *
+ * Return: a pointer to @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
 
- char *_memcpy(char *dest, char *src, unsigned int n)
- {
-     unsigned int i;
-     for (i = 0; i < n; i++)
-     {
-         *(dest + i) = *(src + i);
-     }
-     return (dest);
- }
- 
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
