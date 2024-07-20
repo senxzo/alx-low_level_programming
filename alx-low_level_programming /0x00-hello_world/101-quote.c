f71ae54194db554d3f4d411ccd221c34cfed1a6f
senxzo <innocentaudu2821@gmail.com>
2024-07-19 22:29:23 -0400

    Malloc

---
+++ alx-low_level_programming /0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It prints a message to the console.
+ *
+ * Return: Always returns 1 (success).
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
---
+++ alx-low_level_programming /0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the alphabet in lowercase,
+ * followed by a new line, using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
