7a254564c27bfd2f738e89203e6c6994eead34e0
Innocent Audu <innocentaudu2821@gmail.com>
2024-08-01 13:30:55 -0400

    Data structures, singly linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the pointer to the first node of the list
+ * @str: string to be added to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node;
+    char *str_dup;
+
+    if (str == NULL)
+        return (NULL);
+
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    str_dup = strdup(str);
+    if (str_dup == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->str = str_dup;
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return (new_node);
+}
