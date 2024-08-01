adfc67399eff1b1f64c2c751b6afbf1d4573ddb7
Innocent Audu <innocentaudu2821@gmail.com>
2024-08-01 13:53:50 -0400

    Data structures, singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,35 +3,28 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the pointer to the first node of the list
- * @str: string to be added to the new node
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *str_dup;
+	list_t *new;
+	unsigned int len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	while (str[len])
+		len++;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
-    str_dup = strdup(str);
-    if (str_dup == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
-    new_node->str = str_dup;
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	return (*head);
 }
