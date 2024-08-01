e659cf5c0265f21df84b33cafca6a124a6095f62
Innocent Audu <innocentaudu2821@gmail.com>
2024-08-01 12:09:19 -0400

    Data structures, singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include "lists.h"
-#include "main.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: Pointer to the head of the list_t list.
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list
  *
- * Return: The number of nodes in the list.
+ * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+    size_t count = 0;
 
     while (h != NULL)
     {
@@ -19,9 +18,9 @@ size_t print_list(const list_t *h)
         else
             printf("[%u] %s\n", h->len, h->str);
         
-        node_count++;
         h = h->next;
+        count++;
     }
 
-    return node_count;
+    return count;
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,5 +17,7 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
