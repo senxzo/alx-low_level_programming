2d5395d9cc61594f0d88cf319f31014668969db7
Innocent Audu <innocentaudu2821@gmail.com>
2024-08-06 18:11:02 -0400

    Data structures, singly linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,19 +8,19 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next = NULL;
 
-	while (current != NULL)
-	{
-		next = current->next;  /* Save the next node */
-		current->next = prev;  /* Reverse the current node's pointer */
-		prev = current;        /* Move prev and current one step forward */
-		current = next;
-	}
+    while (current != NULL)
+    {
+        next = current->next;  /* Save the next node */
+        current->next = prev;  /* Reverse the current node's pointer */
+        prev = current;        /* Move prev and current one step forward */
+        current = next;
+    }
 
-	*head = prev;  /* Update head to the new first node */
+    *head = prev;  /* Update head to the new first node */
 
-	return (*head);
+    return (*head);
 }
