1d1e655186d7f33816a3c5681bf29ad01aad748d
Innocent Audu <innocentaudu2821@gmail.com>
2024-08-06 02:36:05 -0400

    Data structures, singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,12 +1,47 @@
 #include "lists.h"
 
 /**
+ * create_new_node - creates a new node with the given value.
+ * @n: integer to be added to the new node.
+ *
+ * Return: the address of the new node, or NULL if it failed.
+ */
+static listint_t *create_new_node(int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
+ * insert_at_b - inserts a new node at the beginning of the list.
+ * @head: double pointer to the head of the list.
+ * @new_node: pointer to the new node to be inserted.
+ *
+ * Return: the address of the new node.
+ */
+static listint_t *insert_at_b(listint_t **head, listint_t *new_node)
+{
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: double pointer to the head of the list.
  * @idx: index where the new node should be added, starting at 0.
  * @n: integer to be added to the new node.
  *
- * Return: the address of the new node, or NULL if it failed or index is invalid.
+ * Return: the address of the new node, or NULL if it failed or index is
+ * invalid.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -16,21 +51,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
-	/* Create new node */
-	new_node = malloc(sizeof(listint_t));
+	new_node = create_new_node(n);
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
 
-	/* Insert at the beginning */
 	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
+		return (insert_at_b(head, new_node));
 
-	/* Traverse to the position before the index */
 	current = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
@@ -42,14 +69,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 
-	/* Check if the position is valid */
 	if (current == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	/* Insert the new node */
 	new_node->next = current->next;
 	current->next = new_node;
 
