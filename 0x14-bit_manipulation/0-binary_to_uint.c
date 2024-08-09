e033b1beea47b2564898b80822f14f2a30ab34e6
Innocent Audu <innocentaudu2821@gmail.com>
2024-08-09 01:55:42 -0400

    Data structures, singly linked list

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string of binary digits (0 and 1).
+ *
+ * Return: The converted number, or 0 if invalid
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		result = (result << 1) | (*b - '0');
+		b++;
+	}
+
+	return (result);
+}
